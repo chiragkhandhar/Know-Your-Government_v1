a435ebb646f8a9155de5845b721578a4ba945395
chiragkhandhar <ckhandhar@hawk.iit.edu>
2020-03-21 18:19:54 -0500

    Basic Skeleton of MainActivity done.

--- app/src/main/java/ml/chiragkhandhar/knowyourgovernment/MainActivity.java
+++ app/src/main/java/ml/chiragkhandhar/knowyourgovernment/MainActivity.java
@@ -1,14 +1,100 @@
 package ml.chiragkhandhar.knowyourgovernment;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    private RecyclerView rv;
+    private TextView location;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupComponents();
+
+        if(networkChecker())
+        {
+            // Do Stuff
+        }
+        else
+        {
+            noNetworkDialog(getString(R.string.networkErrorMsg1));
+        }
+    }
+    public void setupComponents()
+    {
+        rv = findViewById(R.id.recycler);
+        location = findViewById(R.id.location);
+    }
+
+    public boolean networkChecker()
+    {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(cm == null)
+            return false;
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    public void noNetworkDialog(String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setIcon(R.drawable.ic_error);
+        builder.setTitle(R.string.networkErrorTitle);
+        builder.setMessage(message);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.opt_menu,menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.search:
+                if(networkChecker())
+                {
+                    // Some Stuff
+                }
+                else
+                    noNetworkDialog(getString(R.string.networkErrorMsg2));
+                break;
+            case R.id.about:
+                // Some Stuff
+                break;
+            default:
+                Toast.makeText(this,"Invalid Option",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+
     }
 }
